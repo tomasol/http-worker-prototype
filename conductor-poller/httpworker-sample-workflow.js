65ec2fb1f436cfa8660ed15f78f8c9d42dcbcbdd
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-07-07 13:39:18 +0200

    Rewrite httpworker-sample-workflow.js into an integration test

--- conductor-poller/httpworker-sample-workflow.js
+++ conductor-poller/httpworker-sample-workflow.js
@@ -1,5 +1,4 @@
 const ConductorClient = require('conductor-client').default
-const {registerHttpWorker} = require('./conductor-polling');
 const {httpTaskDef, sampleWorkflowDef} = require('../shared/defs');
 
 const conductorClient = new ConductorClient({
@@ -10,29 +9,44 @@ const input = {
     http_request: {
         uri: 'https://httpbin.org/post',
         method: 'POST',
-        headers: {'Content-Type': 'text/html; charset=UTF8'},
-        body: 'some ___SECRET_secret/key1:f1___ data\nAnd another ___SECRET_secret/key2:f2___\n___SECRET_secret/key1:f2___ third',
+        headers: {'Content-Type': 'text/html; charset=UTF8', 'X-Vault-Test':'___SECRET_secret/key1:f1___'},
+        body: `[___SECRET_secret/key1:f1___,___SECRET_secret/key2:f2___,___SECRET_secret/key1:f2___]`,
         timeout: 1000
     }
 };
 
-const input2 = {
-    http_request: {
-        uri: 'https://postman-echo.com/cookies/set?foo1=bar1&foo2=bar2',
-        method: 'GET',
-        timeout: 1000
+function assertEquals(a, b) {
+    if (a!=b) {
+        throw `Assertion error: ${a} != ${b}`;
     }
-};
+}
 
-// update definitions in conductor and start workflow
-conductorClient
-    .registerTaskDefs([httpTaskDef])
-    .then(() =>
-        conductorClient.updateWorkflowDefs([sampleWorkflowDef]).then(() => {
-            conductorClient.startWorkflow(sampleWorkflowDef.name, input).then(workflow => console.log('workflow started, id: ', workflow.data));
-        })
-    )
-    .catch(error => console.dir(error, {depth: 10}))
+function checkResult(response) {
+    assertEquals(response.headers['X-Vault-Test'], '1');
+    const body = response.json;
+    const expectedBody = [1, 20, 2];
+    assertEquals(JSON.stringify(body), JSON.stringify(expectedBody));
+    console.log('All OK');
+}
 
-// start worker
-registerHttpWorker();
+async function main() {
+    await conductorClient.updateWorkflowDefs([sampleWorkflowDef]);
+    const workflowId = (await conductorClient.startWorkflow(sampleWorkflowDef.name, input)).data;
+    console.log('workflow started, id: ', workflowId);
+    for (let iterations = 0; ; iterations++) {
+        if (iterations == 5000) {
+            throw 'Workflow did not complete';
+        }
+        let polled = await conductorClient.getWorkflow(workflowId);
+        if (polled.data.status === 'COMPLETED') {
+            console.log('Checking', polled.data.output.body);
+            const body = JSON.parse(polled.data.output.body);
+            checkResult(body);
+            break;
+        } else {
+            console.log(polled.data.status);
+        }
+    }
+}
+
+main();
